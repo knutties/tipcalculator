e5dbd4248a66671779ff722709e1af5c671f218c
natarajk <natarajk@yahoo-inc.com>
2014-06-03 12:19:18 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Natarajan Kannan on 6/3/14.
+//  Copyright (c) 2014 Y.CORP.YAHOO.COM\natarajk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
